8d90cded11914da7b0aabaaf0ef469cde0dcb18b
Pratheek Kura <kurapr@gmail.com>
2019-11-24 22:22:43 -0500

    commit

--- src/edu/cs3500/spreadsheets/view/ReadOnlyView.java
+++ src/edu/cs3500/spreadsheets/view/ReadOnlyView.java
@@ -1,11 +1,61 @@
 package edu.cs3500.spreadsheets.view;
 
+import java.util.ArrayList;
+
 import edu.cs3500.spreadsheets.model.Cell;
 import edu.cs3500.spreadsheets.model.WorkSheet;
 
 /**
  * Worksheet that is only able to be read, not modified.
  */
-public interface ReadOnlyView extends WorkSheet<Cell> {
+public interface ReadOnlyView<R> {
+
+  /**
+   * Evaluate the value of a Cell at a given position.
+   *
+   * @param row the row of the Cell being evaluated.
+   * @param col the column of the Cell being evaluated.
+   * @return the CellComponent that is in the Cell being called.
+   */
+  public String evaluate(int row, int col);
+
+  /**
+   * Gets the Cell at the given row and col.
+   *
+   * @param row the row of the Cell being returned.
+   * @param col the col of the Cell being returned.
+   * @return The Cell at the given index.
+   */
+  public R getCellAt(int row, int col);
+
+  /**
+   * Gets the String value of the Cell being asked for.
+   *
+   * @param row the row index of the Cell bing asked for.
+   * @param col the column index of the Cell being asked for.
+   * @return the String value of the Cell being asked for by the user.
+   */
+  public String getStringOfCell(int row, int col);
+
+  /**
+   * Gets the state of the current spreadsheet.
+   *
+   * @return a spreadsheet in the for of an List of List of R.
+   */
+  public ArrayList<ArrayList<R>> getSpreadSheet();
+
+  /**
+   * Gets the number of rows in the worksheet.
+   *
+   * @return the number of rows.
+   */
+  public int getNumRows();
+
+  /**
+   * Gets the number of columns in the worksheet.
+   *
+   * @return the number of columns.
+   */
+  public int getNumCols();
 
 }
